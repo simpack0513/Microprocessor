3a14d0405cec72b9d970ee4443b81fa5ed03ca68
simpack0513 <77850439+simpack0513@users.noreply.github.com>
2023-12-13 15:19:08 +0900

    FEEL 기능 구현 완료

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <avr/io.h>
 #define F_CPU 16000000UL
+#define __DELAY_BACKWARD_COMPATIBLE__
 #include <util/delay.h>
 #include <avr/interrupt.h>
 #include <stdio.h>
@@ -7,10 +8,15 @@
 
 #define FREE 0
 #define FEED 1
-#define PLAY 2
-#define FEEL 3
+#define FEEL 2
+#define PLAY 3
 #define ON 1
 #define OFF 0
+#define UP 1
+#define DOWN 0
+
+#define TRIG 6  // 포트 E의 6번핀
+#define ECHO 7  // 포트 E의 7번핀
 
 unsigned char ddrc[4];
 volatile int cnt;
@@ -18,7 +24,11 @@ volatile int status = FREE;
 volatile int wait_time;
 volatile int feed_count = 0;
 volatile int state = OFF;
+volatile int u_state = UP;
+volatile int feel_count = 0;
 
+
+unsigned char fnd[4];
 unsigned char lv[2] = {
 	0x38,
 	0xBE,
@@ -49,11 +59,22 @@ ISR(TIMER0_OVF_vect){
 	
 	if(cnt == 500 * wait_time){
 		if (status == FREE) {
+			int num = rand() % 2 + 1;
 			wait_time = 10;
-			feed_count = 100;
-			status = FEED;
+			if (num == FEED) {
+				feed_count = 100;
+				status = FEED;
+			}
+			else if (num == FEEL) {
+				feel_count = 20;
+				status = FEEL;
+			}
 		}
 		else if (status == FEED) {
+			wait_time = rand() % 7 + 1;
+			status = FREE;
+		}
+		else if (status == FEEL) {
 			wait_time = rand() % 7 + 1;
 			status = FREE;
 		}
@@ -64,7 +85,7 @@ ISR(TIMER0_OVF_vect){
 void init() {
 	DDRC = 0xff;
 	DDRG = 0x0f;
-	DDRE = 0x00;
+	DDRE = ((DDRE|(1<<TRIG)) & ~(1<<ECHO)); 
 	
 	
 	// 타미어 설정
@@ -101,9 +122,40 @@ void show_feed() {
 	}
 }
 
+void show_feel() {
+	ddrc[0] = 0x71;
+	ddrc[1] = 0x79;
+	ddrc[2] = 0x79;
+	ddrc[3] = 0x38;
+	
+	for (int i = 0; i < 4; i++) {
+		PORTC = ddrc[i];
+		PORTG = fnd_sel[i];
+		_delay_ms(2);
+	}
+}
+
+void display_fnd(unsigned int value)
+{
+	int i;
+	fnd[0] = (value/1000)%10;
+	fnd[1] = (value/100)%10;
+	fnd[2] = (value/10)%10;
+	fnd[3] = (value/1)%10;
+	for(i=0; i<4; i++)
+	{
+		PORTC = digit[fnd[i]] | (i==1 ? 0x80 : 0x00);
+		PORTG = fnd_sel[i];
+		_delay_ms(2);
+		if(i%2)
+		_delay_ms(1);
+	}
+}
+
 int main() {
 	init();
 	int level = 0;
+	unsigned int distance;
 	
 	
 	wait_time = 3;
@@ -112,6 +164,47 @@ int main() {
 	
 	
 	while (1) {
+		// 초음파 제어
+		if (status == FEEL) {
+			TCCR1B = 0x02;  // 분주비 설정  0b00000010
+			PORTE &= ~(1<<TRIG);
+			_delay_us(10);
+			PORTE |= (1<<TRIG);
+			_delay_us(10);
+			PORTE &= ~(1<<TRIG);
+
+			while(!(PINE & (1<<ECHO)));
+			TCNT1 = 0x0000;
+			while(PINE & (1<<ECHO)) ;
+
+			TCCR1B = 0x00;
+			distance = (unsigned int)(TCNT1 / 2 / 5.8);
+			if (distance >= 100) {
+				if (u_state == DOWN) {
+					u_state = UP;
+					feel_count -= 1;
+					if (feel_count <= 0) {
+						level += 1;
+						cnt = 0;
+						wait_time = rand() % 7 + 5;
+						status = FREE;
+					}
+				}
+			}
+			else {
+				if (u_state == UP) {
+					u_state = DOWN;
+					feel_count -= 1;
+					if (feel_count <= 0) {
+						level += 1;
+						cnt = 0;
+						wait_time = rand() % 7 + 5;
+						status = FREE;
+					}
+				}
+			}
+		}
+		//초음파 끝
 		
 		// 스위치 제어
 		if ((PINE & 0x10) == 0x00) {
@@ -139,6 +232,9 @@ int main() {
 				break;
 			case FEED:
 				show_feed();
+				break;
+			case FEEL:
+				show_feel();
 				break;
 		}
 	}
