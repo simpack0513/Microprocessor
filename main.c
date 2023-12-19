619accbdac6b0a88cd7367f0498636c0bf8cab97
simpack0513 <77850439+simpack0513@users.noreply.github.com>
2023-12-19 13:55:52 +0900

    자이로 센서 시도중

--- main.c
+++ main.c
@@ -1,6 +1,5 @@
 #include <avr/io.h>
 #define F_CPU 16000000UL
-#define __DELAY_BACKWARD_COMPATIBLE__
 #include <util/delay.h>
 #include <avr/interrupt.h>
 #include <stdio.h>
@@ -18,6 +17,8 @@
 #define TRIG 6  // 포트 E의 6번핀
 #define ECHO 7  // 포트 E의 7번핀
 
+#define FS_SEL 131
+
 unsigned char ddrc[4];
 volatile int cnt;
 volatile int status = FREE;
@@ -26,6 +27,23 @@ volatile int feed_count = 0;
 volatile int state = OFF;
 volatile int u_state = UP;
 volatile int feel_count = 0;
+
+//자이로 관련
+int error_rom = 10;
+int data_rom = 0;
+int read_number = 0;
+unsigned char data;
+unsigned char dataAdress = 0x3B;//가속도 값 부터  각속독 값까지 읽어오는 주소
+unsigned char datainformation[14];
+unsigned int accle_X;
+unsigned int accle_Y;
+unsigned int accle_Z;
+unsigned int temp;
+float ftemp;
+unsigned int angular_X;
+unsigned int angular_Y;
+unsigned int angular_Z;
+//
 
 
 unsigned char fnd[4];
@@ -59,7 +77,7 @@ ISR(TIMER0_OVF_vect){
 	
 	if(cnt == 500 * wait_time){
 		if (status == FREE) {
-			int num = rand() % 2 + 1;
+			int num = rand() % 3 + 1;
 			wait_time = 10;
 			if (num == FEED) {
 				feed_count = 100;
@@ -69,12 +87,19 @@ ISR(TIMER0_OVF_vect){
 				feel_count = 20;
 				status = FEEL;
 			}
+			else if (num == PLAY) {
+				status = PLAY;
+			}
 		}
 		else if (status == FEED) {
 			wait_time = rand() % 7 + 1;
 			status = FREE;
 		}
 		else if (status == FEEL) {
+			wait_time = rand() % 7 + 1;
+			status = FREE;
+		}
+		else if (status == PLAY) {
 			wait_time = rand() % 7 + 1;
 			status = FREE;
 		}
@@ -93,6 +118,10 @@ void init() {
 	TIMSK = (1<<TOIE0);
 	TCNT0 = 131;
 	//
+	
+	//I2C 설정
+	TWBR = 12;//400KHZ 맞추기
+	TWSR = 0x0;
 	
 }
 
@@ -135,6 +164,19 @@ void show_feel() {
 	}
 }
 
+void show_play() {
+	ddrc[0] = 0x73;
+	ddrc[1] = 0x38;
+	ddrc[2] = 0x77;
+	ddrc[3] = 0x6E;
+	
+	for (int i = 0; i < 4; i++) {
+		PORTC = ddrc[i];
+		PORTG = fnd_sel[i];
+		_delay_ms(2);
+	}
+}
+
 void display_fnd(unsigned int value)
 {
 	int i;
@@ -157,13 +199,17 @@ int main() {
 	int level = 0;
 	unsigned int distance;
 	
-	
 	wait_time = 3;
-	
 	sei(); // 타이머
 	
 	
 	while (1) {
+		// 자이로 제어
+		if (status == PLAY) {
+			mpu6050_Write();
+			mpu6050_read();
+		}
+		
 		// 초음파 제어
 		if (status == FEEL) {
 			TCCR1B = 0x02;  // 분주비 설정  0b00000010
@@ -236,6 +282,84 @@ int main() {
 			case FEEL:
 				show_feel();
 				break;
+			case PLAY:
+				show_play();
+				break;
 		}
 	}
+}
+
+
+//자이로센서 관련 함수
+
+void mpu6050_Start(void){
+	TWCR = 0xA4;//명령으로 줘서 TWINT 1 -> 0(자동클리어)이 되가지고 SCL 값이 High, SDA, SCL 유효
+	//START MASTER 지정
+	while(!(TWCR&0x80));//TWINT 0 -> 1 이 되가지고 SCL 값이 0 이 될떄까지 기다림.
+	if((TWSR & 0xF8) != 0x08)Error((TWSR & 0xF8)); //스타트 문제 검출
+}
+void mpu6050_SLA_W(void){//들어가기 위한 비밀번호(SLA+W)
+	TWDR = 0xD0;//무언가의 값을 쓸때 0xD0,mpu6050 읽을 떄  0xD1
+	TWCR = 0x84;//명령으로 줘서 TWINT 1 -> 0(자동클리어)이 되가지고 SCL 값이 High, SDA, SCL 유효
+	while(!(TWCR&0x80));//TWINT 0 -> 1 이 되가지고 SCL 값이 0 이 될떄까지 기다림.
+	if((TWSR & 0xF8) != 0x18)Error((TWSR & 0xF8)); //SLA+W 문제 검출
+	
+	TWDR = 1;//어느 방에 쓰거나 읽을걸지
+	TWCR = 0x84;//명령으로 줘서 TWINT 1 -> 0(자동클리어)이 되가지고 SCL 값이 High, SDA, SCL 유효
+	while(!(TWCR&0x80));//TWINT 0 -> 1 이 되가지고 SCL 값이 0 이 될떄까지 기다림.
+	if((TWSR & 0xF8) != 0x28)Error((TWSR & 0xF8)); //SLA+W 문제 검출
+}
+void mpu6050_Write(void){
+	mpu6050_Start();
+	mpu6050_SLA_W();
+	TWDR = 'a';//무언가의 값을 쓸때 0xD0,mpu6050 읽을 떄  0xD1
+	TWCR = 0x84;//명령으로 줘서 TWINT 1 -> 0(자동클리어)이 되가지고 SCL 값이 High, SDA, SCL 유효
+	while(!(TWCR&0x80));//TWINT 0 -> 1 이 되가지고 SCL 값이 0 이 될떄까지 기다림.
+	if((TWSR & 0xF8) != 0x28)Error((TWSR & 0xF8)); //데이터 쓸 때  문제 검출
+	mpu6050_STOP();
+}
+void mpu6050_read(void){
+	mpu6050_Start();
+	mpu6050_SLA_W();
+	TWCR = 0xA4;//명령으로 줘서 TWINT 1 -> 0(자동클리어)이 되가지고 SCL 값이 High, SDA, SCL 유효
+	//START MASTER 지정
+	while(!(TWCR&0x80));//TWINT 0 -> 1 이 되가지고 SCL 값이 0 이 될떄까지 기다림.
+	if((TWSR & 0xF8) != 0x10)Error((TWSR & 0xF8)); //스타트 문제 검출
+	
+	TWDR = 0xD1;//무언가의 값을 쓸때 0xD0,mpu6050 읽을 떄  0xD1
+	TWCR = 0x84;//명령으로 줘서 TWINT 1 -> 0(자동클리어)이 되가지고 SCL 값이 High, SDA, SCL 유효
+	while(!(TWCR&0x80));//TWINT 0 -> 1 이 되가지고 SCL 값이 0 이 될떄까지 기다림.
+	if((TWSR & 0xF8) != 0x40)Error((TWSR & 0xF8)); //SLA+W 문제 검출
+	
+	TWCR = 0x84;//명령으로 줘서 TWINT 1 -> 0(자동클리어)이 되가지고 SCL 값이 High, SDA, SCL 유효
+	while(!(TWCR&0x80));//TWINT 0 -> 1 이 되가지고 SCL 값이 0 이 될떄까지 기다림.
+	if((TWSR & 0xF8) != 0x58)Error((TWSR & 0xF8)); //읽을 준비 검출
+	data = TWDR;
+	
+	mpu6050_STOP();
+	
+	while((EECR & 0x02) == 0x02); //받은 값 방 0번부터 시작해서 저장
+	EEAR = data_rom++;
+	EEDR = data;
+	EECR |= 0x04;
+	EECR |= 0x02;
+}
+void mpu6050_STOP(void){//들어가기 위한 비밀번호(SLA+W)
+	
+	TWCR = 0x94;//명령으로 줘서 TWINT 1 -> 0(자동클리어)이 되가지고 SCL 값이 High, SDA, SCL 유효
+	//STOP 기능한다.
+	_delay_ms(1000);
+}
+void Error(char error){
+	if(error == 0x20)PORTC = 0x01;//SLA+W 신호송신후 확인신호가 없음
+	else if(error == 0x38)PORTC = 0x02;// SLA+W 나 데이터 신호 후 문제 발생
+	else if(error == 0x30)PORTC = 0x04;//테이터 신호 송신 후 확인신호가 순신되지 않음
+	else PORTC = 0xFF;// TWI_START오류거나 그 외의 오류
+	
+	while((EECR & 0x02) == 0x02); //에러부분 저장 방 10번부터
+	EEAR = error_rom++;
+	EEDR = error;
+	EECR |= 0x04;
+	EECR |= 0x02;
+	while(1);
 }
